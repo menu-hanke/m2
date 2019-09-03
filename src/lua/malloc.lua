4392e178e764c62af9e8541983c0b944349c3fc5
tl <tl@cat.pm>
2019-09-03 10:00:24 +0300

    lua/malloc: gc by default

--- src/lua/malloc.lua
+++ src/lua/malloc.lua
@@ -5,8 +5,16 @@ ffi.cdef [[
 	void free(void *ptr);
 ]]
 
+-- This is mainly useful for allocating long-living cdata that need to be referenced from C,
+-- since gc will move cdata allocated with ffi.new
+local function malloc_ctype(ct, nelem)
+	nelem = nelem or 1
+	return ffi.cast(ct .. "*", ffi.C.malloc(ffi.sizeof(ct) * nelem))
+end
+
 return {
+	new_nogc = malloc_ctype,
 	new = function(ct, nelem)
-		return ffi.cast(ct .. "*", ffi.C.malloc(ffi.sizeof(ct) * nelem))
+		return ffi.gc(malloc_ctype(ct, nelem), ffi.C.free)
 	end
 }
